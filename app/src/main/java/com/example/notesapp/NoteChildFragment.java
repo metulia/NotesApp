255e17e197b7820b51c512887f6fba544310401b
Yuliia Stepkina <metulia@mail.ru>
2022-12-05 15:52:40 +0300

    added button to browse gb.ru site in NoteChildFragment

--- app/src/main/java/com/example/notesapp/NoteChildFragment.java
+++ app/src/main/java/com/example/notesapp/NoteChildFragment.java
@@ -1,12 +1,17 @@
 package com.example.notesapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NoteChildFragment extends Fragment {
 
@@ -24,5 +29,21 @@ public class NoteChildFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note_child, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final TextView site = view.findViewById(R.id.tv_note_child);
+        site.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = site.getText().toString();
+                Uri uri = Uri.parse(url);
+                Intent browser = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(browser);
+            }
+        });
     }
 }
