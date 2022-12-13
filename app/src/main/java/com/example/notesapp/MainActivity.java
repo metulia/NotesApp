0fb6d2d423f27a5011f837eb808ccd30865f404c
Yuliia Stepkina <metulia@mail.ru>
2022-12-13 13:53:16 +0300

    added options_menu

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.notesapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,31 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .add(R.id.notes_container, new NotesFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_calendar:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .replace(R.id.notes_container, new CalendarFragment())
+                        .commit();
+                return true;
+
+            case R.id.action_exit:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
