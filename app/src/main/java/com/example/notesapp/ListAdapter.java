bbc734943e2dd2b49d045f3a2cf26160f43fe5ca
Yuliia Stepkina <metulia@mail.ru>
2022-12-21 17:28:34 +0300

    added changes to ListFragment.java

--- app/src/main/java/com/example/notesapp/ListAdapter.java
+++ app/src/main/java/com/example/notesapp/ListAdapter.java
@@ -1,6 +1,58 @@
 package com.example.notesapp;
 
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ListAdapter {
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
+    NotesSource dataSource;
+
+    public ListAdapter(NotesSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @NonNull
+    @Override
+    public ListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item, viewGroup, false);
+        return new ListAdapter.ViewHolder(v);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setData(dataSource.getNoteData(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSource.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView noteTitle;
+        private TextView noteDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            noteTitle = itemView.findViewById(R.id.note_title);
+            noteDescription = itemView.findViewById(R.id.note_description);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        public void setData(Note note) {
+            noteTitle.setText(note.getTitle());
+            noteDescription.setText(note.getDescription());
+        }
+    }
 }
--- app/src/main/java/com/example/notesapp/ListFragment.java
+++ app/src/main/java/com/example/notesapp/ListFragment.java
@@ -1,14 +1,21 @@
 package com.example.notesapp;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ListFragment extends Fragment {
+
+    public static ListFragment newInstance() {
+        return new ListFragment();
+    }
 
     public ListFragment() {
         // Required empty public constructor
@@ -19,10 +26,25 @@ public class ListFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_lines);
+
+        NotesSource data = new NotesSourceImpl(getResources()).init();
+
+        initRecyclerView(recyclerView, data);
+
+        return view;
+    }
+
+    private void initRecyclerView(RecyclerView recyclerView, NotesSource data) {
+
+        ListAdapter listAdapter = new ListAdapter(data);
+        recyclerView.setAdapter(listAdapter);
     }
 }
