47336f9d30ea9a17da3d4a025830896bc9b13d2d
Yuliia Stepkina <metulia@mail.ru>
2022-12-23 15:26:52 +0300

    added some changes

--- app/src/main/java/com/example/notesapp/ListFragment.java
+++ app/src/main/java/com/example/notesapp/ListFragment.java
@@ -3,6 +3,7 @@ package com.example.notesapp;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -10,6 +11,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,12 +21,41 @@ import android.widget.Toast;
 
 public class ListFragment extends Fragment {
 
+    private NotesSource data;
+    private ListAdapter adapter;
+    private RecyclerView recyclerView;
+
     public static ListFragment newInstance() {
         return new ListFragment();
     }
 
     public ListFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.notes_menu, menu);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                data.addNote(new Note("Новая заметка #" + data.size(), "Описание заметки #" + data.size(),
+                        R.drawable.nature11, false));
+                adapter.notifyItemInserted(data.size() - 1);
+                recyclerView.scrollToPosition(data.size() - 1);
+                return true;
+
+            case R.id.action_clear:
+                data.clear();
+                adapter.notifyDataSetChanged();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -37,26 +70,32 @@ public class ListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
-        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_lines);
-
-        NotesSource data = new NotesSourceImpl(getResources()).init();
-
-        initRecyclerView(recyclerView, data);
+        initView(view);
+        setHasOptionsMenu(true);
 
         return view;
     }
 
-    private void initRecyclerView(RecyclerView recyclerView, NotesSource data) {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void initView(View view) {
 
-        ListAdapter listAdapter = new ListAdapter(data);
-        recyclerView.setAdapter(listAdapter);
+        recyclerView = view.findViewById(R.id.recycler_view_lines);
+        data = new NotesSourceImpl(getResources()).init();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+
+        adapter = new ListAdapter(data);
+
+        recyclerView.setAdapter(adapter);
 
         DividerItemDecoration itemItemDecoration = new
                 DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL);
         itemItemDecoration.setDrawable(getResources().getDrawable(R.drawable.separator, null));
         recyclerView.addItemDecoration(itemItemDecoration);
 
-        listAdapter.setItemClickListener(new OnItemClickListener() {
+        adapter.setItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
                 Toast.makeText(getContext(), String.format("Позиция - %d", position),
